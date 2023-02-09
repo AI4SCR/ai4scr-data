4cf6480896329572b0dd6aad02e038974ae343c1
Adriano Martinelli <art@zurich.ibm.com>
2023-02-09 13:10:04 +0100

    clean up and prepare for publish

--- setup.py
+++ setup.py
@@ -1,83 +1,10 @@
-"""Install package."""
-import io
-import re
+# configuration approach followed:
+# - whenever possible, prefer pyproject.toml
+# - for configurations insufficiently supported by pyproject.toml, use setup.cfg instead
+# - setup.py discouraged; minimal stub included only for compatibility with legacy tools
 
-from pkg_resources import parse_requirements
-from setuptools import find_packages, setup
+# Minimal stub for backwards compatibility, e.g. for legacy tools without PEP 660 support.
+# See https://setuptools.pypa.io/en/latest/userguide/quickstart.html
+from setuptools import setup
 
-
-def read_version(filepath: str) -> str:
-    """Read the __version__ variable from the file.
-
-    Args:
-        filepath: probably the path to the root __init__.py
-
-    Returns:
-        the version
-    """
-    match = re.search(
-        r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-        io.open(filepath, encoding="utf_8_sig").read(),
-    )
-    if match is None:
-        raise SystemExit("Version number not found.")
-    return match.group(1)
-
-
-# ease installation during development
-vcs = re.compile(r"(git|svn|hg|bzr)\+")
-try:
-    with open("requirements.txt") as fp:
-        VCS_REQUIREMENTS = [
-            str(requirement)
-            for requirement in parse_requirements(fp)
-            if vcs.search(str(requirement))
-        ]
-except FileNotFoundError:
-    # requires verbose flags to show
-    print("requirements.txt not found.")
-    VCS_REQUIREMENTS = []
-
-setup(
-    name="ai4scr-dataset",
-    version=read_version("dataset/__init__.py"),  # single place for version
-    description="Generic dataset package.",
-    long_description=open("README.md").read(),
-    url="https://github.ibm.com/AI4SCR-DEV/dataset",
-    author="Adriano Martinelli",
-    author_email="art@zurich.ibm.com",
-    # the following exclusion is to prevent shipping of tests.
-    # if you do include them, add pytest to the required packages.
-    packages=find_packages(".", exclude=["*tests*"]),
-    package_data={"dataset": ["py.typed"]},
-    entry_points="""
-        [console_scripts]
-    """,
-    scripts=[],
-    extras_require={
-        "vcs": VCS_REQUIREMENTS,
-        "test": ["pytest", "pytest-cov"],
-        "dev": [
-            # tests
-            "pytest",
-            "pytest-cov",
-            # checks
-            "black==21.5b0",
-            "flake8",
-            "mypy",
-            # docs
-            "sphinx",
-            "sphinx-autodoc-typehints",
-            "better-apidoc",
-            "six",
-            "sphinx_rtd_theme",
-            "myst-parser",
-        ],
-    },
-    install_requires=[
-        # versions should be very loose here, just exclude unsuitable versions
-        # because your dependencies also have dependencies and so on ...
-        # being too strict here will make dependency resolution harder
-        'tqdm'
-    ],
-)
+setup()
